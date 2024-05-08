0216557d703453bf34d37943f141232bf9a5e62e
m.kulik <camelot1399@gmail.com>
2024-05-08 18:06:09 +0300

    changed card

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,3 +1,16 @@
+import style from "./Card.module.scss";
+
 export const Card = () => {
-  return <div>Card</div>;
+  {
+    /* TODO написать коомпонент вывода товара - структуру дива не меняем, только название, категорию и цену  */
+  }
+  return (
+    <div className={style.card}>
+      <div className={style.card__wrapper}>
+        <div>Название: клавиатура</div>
+        <div>Категория: техника</div>
+        <div>Цена: 1000 р.</div>
+      </div>
+    </div>
+  );
 };
