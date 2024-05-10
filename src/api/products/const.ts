733adf3b7c550c626637bfdd5d640540b40cf603
Sergey Skvorcove <skvorcovsa90@gmail.com>
2024-05-10 17:02:53 +1000

    add test with real API

--- src/api/products/const.ts
+++ src/api/products/const.ts
@@ -21,3 +21,26 @@ export const keyValueMappings: KeyValueMappings = {
     [FRONT__CATEGORY]: [BACK__CATEGORY],
     [FRONT__PRICE]: [BACK__PRICE]
 }
+
+// проверочный код работы с реальным API
+// раскомментируйте блок ниже и закомментируйте верхний блок
+
+// const BASE_URL = "https://fakestoreapi.com/";
+// export const PRODUCTS_URL = BASE_URL + "products";
+
+// export const FRONT__TITLE = "title";
+// export const FRONT__CATEGORY = "category";
+// export const FRONT__PRICE = "price";
+
+// // этот эндпоинт использует следующие названия
+// export const BACK__TITLE = "title";
+// export const BACK__CATEGORY = "category";
+// export const BACK__PRICE = "price";
+
+// export const keyValueMappings: KeyValueMappings = {
+//     [FRONT__TITLE]: [BACK__TITLE],
+//     [FRONT__CATEGORY]: [BACK__CATEGORY],
+//     [FRONT__PRICE]: [BACK__PRICE]
+// }
+
+// Как видим, мы поменяли только названия свойст бека и сам эндпоинт и у нас всё прекрасно работает
